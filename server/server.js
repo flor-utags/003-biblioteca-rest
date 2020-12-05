a8dd39b8c5591bc6cd398edf6f897a78f0ebab84
flor.rangel <flor.rangel@hotmail.com>
2020-12-04 22:34:12 -0600

    Login con password encriptado

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(require('./routes/categoria'));
 app.use(require('./routes/producto'));
 app.use(require('./routes/login'));
 
-mongoose.connect('mongodb://localhost:27017/cafeteria', {
+mongoose.connect('mongodb+srv://admin:12345678ms@cluster0.74gdo.mongodb.net/cafeteria', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
